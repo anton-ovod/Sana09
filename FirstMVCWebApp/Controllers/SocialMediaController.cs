8c2ca91915fd4d6cef61ab097c01b8f736ff2e43
Anton Ovod <antovod36@gmail.com>
2024-03-14 20:16:21 +0100

    Added controller for social media

---
+++ FirstMVCWebApp/Controllers/SocialMediaController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FirstMVCWebApp.Controllers
+{
+    public class SocialMediaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Facebook()
+        {
+            return View();
+        }
+
+        public IActionResult Twitter()
+        {
+            return View();
+        }
+
+        public IActionResult Instagram()
+        {
+            return View();
+        }
+    }
+}
